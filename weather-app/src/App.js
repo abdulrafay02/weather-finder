3a2039a02259b3a4d0b93de349ac3a3172a712da
abdulrafay02 <abdulrafay_02@live.com>
2018-09-18 13:52:28 +0500

    adding project to github

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Titles from "./components/titles"
-import Form from "./components/form"
-import Weather from "./components/weather"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Titles from "./components/titles";
+import Form from "./components/form";
+import Weather from "./components/weather";
 
-const API_KEY = "0699f56c5ec2ad6108438915a16306e1";
+const API_KEY = "insert_your_api_key_here";
 
 class App extends Component {
-
   state = {
     temperature: undefined,
     city: undefined,
@@ -16,13 +15,15 @@ class App extends Component {
     humidity: undefined,
     description: undefined,
     error: undefined
-  }
+  };
 
-  getWeather = async (e) => {
+  getWeather = async e => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const apiCall = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&id=524901&APPID=${API_KEY}&units=metric`);
+    const apiCall = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&id=524901&APPID=${API_KEY}&units=metric`
+    );
     const data = await apiCall.json();
     if (city && country) {
       console.log(data);
@@ -44,15 +45,15 @@ class App extends Component {
         error: "Please enter the values"
       });
     }
-  }
+  };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Weather App</h1>
-        </header>
+          <h1 className="App-title">React - Weather Finder</h1>
+        </header> */}
         <div className="wrapper">
           <div className="main">
             <div className="container">
@@ -78,7 +79,6 @@ class App extends Component {
       </div>
     );
   }
-
 }
 
 export default App;
